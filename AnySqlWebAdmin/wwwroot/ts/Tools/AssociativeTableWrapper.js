4ec0f73aea2f01495af09d30688812ca95d7dee9
ststeiger <quandary82@hailmail.net>
2018-09-21 08:52:16 +0200

    TableWrapper

---
+++ AnySqlWebAdmin/wwwroot/ts/Tools/AssociativeTableWrapper.js
@@ -0,0 +1,20 @@
+var associativeTable2 = [
+    { "col1": "value", "col2": "value", "col3": "value3" },
+    { "col1": "value", "col2": "value", "col3": "value3" },
+    { "col1": "value", "col2": "value", "col3": "value3" }
+];
+var associativeTable = [
+    { "col1": "value", "col2": "value", "col3": "value3" },
+    { "col1": "value", "col2": "value", "col3": "value3" },
+    { "col1": "value", "col2": "value", "col3": "value3" }
+];
+var AssociativeTableWrapper1 = (function () {
+    function AssociativeTableWrapper1() {
+    }
+    AssociativeTableWrapper1.GetTable = function () {
+        var obj = [];
+        return obj;
+    };
+    return AssociativeTableWrapper1;
+}());
+AssociativeTableWrapper1.GetTable();
