1cc810d80297829b8ba99e483c23d890a38db74f
ststeiger <quandary82@hailmail.net>
2019-02-22 17:13:38 +0100

    Added comparison value

--- AnySqlWebAdmin/TypeScript/SpatialDistance/_main.test.ts
+++ AnySqlWebAdmin/TypeScript/SpatialDistance/_main.test.ts
@@ -23,3 +23,19 @@ let lng2 = 9.556946;
 
 
 let distance1 = SpatialDistanceBetweenPlaces(lat1, lng1, lat2, lng2);
+
+
+// Compare this with the SQL-value
+let sql = `
+DECLARE @fab geography;   
+SET @fab = geography::Point(47.552063, 9.226081, 4326)  
+
+DECLARE @sg geography;   
+SET @sg = geography::Point(47.374487, 9.556946, 4326)  
+
+
+SELECT @fab.STDistance(@sg), @sg.STDistance(@fab) -- 31813.1626618977
+
+-- SELECT 31.813162661481979*1000
+-- 31.813162661481979
+`;
