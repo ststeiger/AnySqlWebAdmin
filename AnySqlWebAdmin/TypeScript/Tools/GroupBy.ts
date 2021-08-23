0f92ac4cb3b3d4fc79e52d8556180c13561b006b
ststeiger <quandary82@hailmail.net>
2021-08-23 18:06:49 +0200

    Added group by as in Linq

---
+++ AnySqlWebAdmin/TypeScript/Tools/GroupBy.ts
@@ -0,0 +1,60 @@
+
+interface Group {
+    key: any;
+    items: any[];
+}
+
+interface GroupBy {
+    keys: string[];
+    thenby?: GroupBy;
+}
+
+function groupBy(array: any[], grouping: GroupBy): Group[]
+{
+    const keys = grouping.keys;
+    const groups = array.reduce((groups, item) => {
+        const group = groups.find((g: { key: { [x: string]: any; }; }) => keys.every(key => item[key] === g.key[key]));
+        const data = Object.getOwnPropertyNames(item)
+            .filter(prop => !keys.find(key => key === prop))
+            .reduce((o, key) => ({ ...o, [key]: item[key] }), {});
+        return group
+            ? groups.map((g: { items: any; }) => (g === group ? { ...g, items: [...g.items, data] } : g))
+            : [
+                ...groups,
+                {
+                    key: keys.reduce((o, key) => ({ ...o, [key]: item[key] }), {}),
+                    items: [data]
+                }
+            ];
+    }, []);
+    return grouping.thenby ? groups.map((g: { items: any[]; }) => ({ ...g, items: groupBy(g.items, grouping.thenby) })) : groups;
+}
+
+
+type Predicate<T, TKey> = (item: T) => TKey;
+
+
+function LinqGroupBy<TSource, TKey>(source: TSource[], keySelector: Predicate<TSource, TKey>)
+    : { [key: string]: TSource[] }
+{
+    if (source == null)
+        throw new Error("ArgumentNullException: Source");
+    if (keySelector == null)
+        throw new Error("ArgumentNullException: keySelector");
+
+    let dict: { [key: string]: TSource[]} = {};
+
+    for (let i = 0; i < source.length; ++i)
+    {
+        let key: string = keySelector(source[i]).toString();
+
+        if (!dict.hasOwnProperty(key))
+        {
+            dict[key] = [];
+        }
+
+        dict[key].push(source[i]);
+    }
+
+    return dict;
+}
---
+++ AnySqlWebAdmin/wwwroot/ts/Tools/GroupBy.js
@@ -0,0 +1,70 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __read = (this && this.__read) || function (o, n) {
+    var m = typeof Symbol === "function" && o[Symbol.iterator];
+    if (!m) return o;
+    var i = m.call(o), r, ar = [], e;
+    try {
+        while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
+    }
+    catch (error) { e = { error: error }; }
+    finally {
+        try {
+            if (r && !r.done && (m = i["return"])) m.call(i);
+        }
+        finally { if (e) throw e.error; }
+    }
+    return ar;
+};
+var __spread = (this && this.__spread) || function () {
+    for (var ar = [], i = 0; i < arguments.length; i++) ar = ar.concat(__read(arguments[i]));
+    return ar;
+};
+function groupBy(array, grouping) {
+    var keys = grouping.keys;
+    var groups = array.reduce(function (groups, item) {
+        var group = groups.find(function (g) { return keys.every(function (key) { return item[key] === g.key[key]; }); });
+        var data = Object.getOwnPropertyNames(item)
+            .filter(function (prop) { return !keys.find(function (key) { return key === prop; }); })
+            .reduce(function (o, key) {
+            var _a;
+            return (__assign(__assign({}, o), (_a = {}, _a[key] = item[key], _a)));
+        }, {});
+        return group
+            ? groups.map(function (g) { return (g === group ? __assign(__assign({}, g), { items: __spread(g.items, [data]) }) : g); })
+            : __spread(groups, [
+                {
+                    key: keys.reduce(function (o, key) {
+                        var _a;
+                        return (__assign(__assign({}, o), (_a = {}, _a[key] = item[key], _a)));
+                    }, {}),
+                    items: [data]
+                }
+            ]);
+    }, []);
+    return grouping.thenby ? groups.map(function (g) { return (__assign(__assign({}, g), { items: groupBy(g.items, grouping.thenby) })); }) : groups;
+}
+function LinqGroupBy(source, keySelector) {
+    if (source == null)
+        throw new Error("ArgumentNullException: Source");
+    if (keySelector == null)
+        throw new Error("ArgumentNullException: keySelector");
+    var dict = {};
+    for (var i = 0; i < source.length; ++i) {
+        var key = keySelector(source[i]).toString();
+        if (!dict.hasOwnProperty(key)) {
+            dict[key] = [];
+        }
+        dict[key].push(source[i]);
+    }
+    return dict;
+}
